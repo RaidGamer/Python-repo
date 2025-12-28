54af2cd6a3cff5639c3f8b6b396bf7694326ac80
Alex (Ubuntu) <alexander.olivius@outlook.com>
2025-12-29 00:04:38 +0100

    Förbättrat på min andra webskrapar version, ska göra den mer generaliserad och bättre på felhantering.

--- Web_scraper/web_scraper2.py
+++ Web_scraper/web_scraper2.py
@@ -2,3 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 from sys import argv, exit
 from urllib.parse import urlparse
+import time 
+import random
+
+#TODO: implementera json lagring
+            
+usage = "="*40+"\nUsage: python3 <script path> <url>\n"+"="*40
+try: url_inp = argv[1] 
+except IndexError: print(usage); exit(2)
+
+agent = "MyWebScraper1.0 (learning project using python; non-commerical use)"
+
+def soup(url: str): #TODO: hantera error http error codes som 429 403
+    headers = {"User-Agent": agent}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
+    return soup
+
+def delay_timer(): 
+    return random.randint(3,5)
+
+def robots(url: str):
+    time.sleep(delay_timer())
+    robot_content = soup(url_inp).get_text
+    #TODO: kolla efter #strings som säger vad som tillåts resp. inte
+
+
+def main():
+    html = soup(url_inp)
+    #TODO: ta fram en massa tags
+
+    
+    
+
+if __name__ == "__main__":
+    main()
